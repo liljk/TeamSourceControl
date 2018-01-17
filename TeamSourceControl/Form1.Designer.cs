053d5325d815b016cfa20527826fa2b9492e22d6
taylorjnickle <31074771+taylorjnickle@users.noreply.github.com>
2018-01-17 14:43:40 -0800

    Created Login page for grade checker

--- TeamSourceControl/Form1.Designer.cs
+++ TeamSourceControl/Form1.Designer.cs
@@ -97,9 +97,9 @@
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FrmLogin";
-            this.Text = "Log in to Grades";
+            this.Text = "Log in";
             this.ResumeLayout(false);
             this.PerformLayout();
 
