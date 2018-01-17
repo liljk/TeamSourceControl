99650459f741c76bfa751c4fe8c54ac51c8aef18
jhoon kwak <liljk@users.noreply.github.com>
2018-01-17 12:54:50 -0800

    Created Student Database.

---
+++ GradeDB/Student.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE dbo.Student
+(
+    --Student ID
+	StudentID int NOT NULL
+	--Student First Name
+	,StudentFName varchar NOT NULL
+	--Student Last Name
+	,StudentLName varchar NOT NULL
+	--Student Birth date
+	,BirthDate datetime NULL
+	--Student Address
+	,StudentAddress varchar NULL
+)
